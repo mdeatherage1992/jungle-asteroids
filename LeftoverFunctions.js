777611436e340fe53f295d83b487851cfc9ce432
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-30 09:08:05 -0400

    white hot explosion

---
+++ LeftoverFunctions.js
@@ -0,0 +1,12 @@
+// This is an assortment of functions that I used, and wanted to keep. Descriptions are accurate
+
+
+//lime green collision
+// function explodeShip() {
+//   ctx.fillStyle = "lime";
+//   ctx.strokeStyle = "lime";
+//   ctx.beginPath();
+//   ctx.arc(ship.x,ship.y,ship.r, 0, Math.PI * 2, false);
+//   ctx.fill();
+//   ctx.stroke();
+// }
